d275d94a55cd8107af824699180743fe4cbf270b
Salvi Pascual <salvi.pascual@gmail.com>
2019-05-06 12:02:17 -0400

    fix service

--- service.php
+++ service.php
@@ -1,64 +1,26 @@
 <?php
 
-
 class Service
 {
 	/**
 	 * Get a prize for women's day celebration
 	 *
-	 * @author alexmasters1
+	 * @author salvipascual
 	 * @param Request
 	 * @param Response
 	 */
 	public function _main(Request $request, Response &$response)
 	{
-        // get the the gender
-        if ($request->person->gender == "M")
-        return $response->setTemplate('message.ejs', [
-            "header" => "Este es un servicio especial para mujeres",
-            "icon"   => "sentiment_satisfied",
-            "text"   => "Hay muchos otros servicios que puedes disfrutar",
-        ]);
+		// check if you are female
+		$isFemale = $request->person->gender == "F";
 
-        // get the survey details
-		$checkSurvey = Connection::query("
-        SELECT 
-            COUNT(*) AS count
-        FROM _survey_answer_choosen 
-        WHERE survey=28 and email='{$request->person->email}'
-        ");
-        
-        if ($checkSurvey[0]->count < 20) {
-            $checkSurvey = false;
-        } else {
-            $checkSurvey = true;
-        }
+		// check if you responded the survey
+		$isSurvey = Connection::query("
+			SELECT COUNT(*) AS count
+			FROM _survey_answer_choosen 
+			WHERE survey=28 and email='{$request->person->email}'")[0]->count >= 20;
 
-        // select images
-        $pathToService = Utils::getPathToService($response->serviceName);
-        $img = "$pathToService/images/premios.jpg";
-        $img2 = "$pathToService/images/simcard1.png";
-        $img3 = "$pathToService/images/recarga.png";
-
-        // create a json object to send to the template
-        $responseContent = array(
-            "checkSurvey"=> $checkSurvey,
-            "img"        => $img,
-            "img2"       => $img2,
-            "img3"       => $img3
-        );
-        
-        // get the images to send to the template
-        $images = array( 
-            $responseContent['img'],
-            $responseContent['img2'],
-            $responseContent['img3']
-        );
-
-        // send data to the view
-		$response->setLayout("oracion.ejs");
-		$response->setTemplate("main.ejs", $responseContent, $images);
-		
-        }
-    }
-
+		// send data to the view
+		$response->setTemplate("main.ejs", ["isFemale"=>$isFemale, "isSurvey"=>$isSurvey]);
+	}
+}
